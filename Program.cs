6db8b718acba8171e15b32c67dcee1c0aabcd664
Taha2341 <86522804+Taha2341@users.noreply.github.com>
2021-07-27 17:20:48 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ödev1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sınav1 = 70, sınav2 = 80, sınav3 = 90;
+            
+            if (sınav1 < sınav2 && sınav1 < sınav3)
+                Console.Write("En düşük sınav={0}", sınav1);
+            else if (sınav2 < sınav1 && sınav2 < sınav3)
+                Console.Write("En düşük sınav={0}", sınav2);
+            else if (sınav3 < sınav1 && sınav3 < sınav2)
+                Console.Write("En düşük sınav={0}", sınav3);
+
+            Console.ReadKey();
+        }
+    }
+}
